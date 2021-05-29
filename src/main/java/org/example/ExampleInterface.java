35d76b09133111e5ae6a625877b9622734b2b415
Apoorva Srivastava <apoorva2398@gmail.com>
2021-05-29 02:02:18 -0400

    Delete unnecessary files

--- src/main/java/org/example/ExampleInterface.java
+++
@@ -1,5 +0,0 @@
-package org.example;
-
-public interface ExampleInterface {
-    String x = "x";
-}
--- src/main/java/org/example/Test1.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Test1 {
-    private final Test2 test2 = new Test2();
-
-    public Test2 getTest2Instance() {
-        return test2;
-    }
-}
