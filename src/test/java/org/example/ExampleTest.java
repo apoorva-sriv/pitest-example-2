ae0803c9ebb848db9ff34c7e62ca4613647a629d
Apoorva Srivastava <apoorva2398@gmail.com>
2021-05-29 02:05:20 -0400

    Optimize imports

--- src/test/java/org/example/ExampleTest.java
+++ src/test/java/org/example/ExampleTest.java
@@ -1,13 +1,8 @@
 package org.example;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Answers;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.when;
 
 public class ExampleTest {
 
